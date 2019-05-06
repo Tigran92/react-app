9e21508acf7fb7748e63af6a07d2b6be1d5417b2
Tigran Grigoryan <tigrangrigoryan@Tigrans-MacBook-Air.local>
2019-05-07 02:53:31 +0400

    Event handlers

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -7,18 +7,43 @@ class Counter extends Component {
     // imageUrl: "http://picsum.photos/300"
   };
 
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
+
+    return (
+      <ul>
+        {this.state.tags.map(tag => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
+    );
+  }
+
+  // constructor() {
+  //   super();
+  //   this.handleIncrement = this.handleIncrement.bind(this);
+  // }
+
+  handleIncrement = product => {
+    console.log(product);
+    console.log("Increment Clicked", this.state.count);
+    this.setState({ count: this.state.count + 1 });
+  };
+
   render() {
     //let classes = this.getBadgeClasses();
     return (
       <div>
         {/* <img src={this.state.imageUrl } alt="" /> */}
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
-        <ul>
-          {this.state.tags.map(tag => (
-            <li key={tag}>{tag}</li>
-          ))}
-        </ul>
+        <button
+          onClick={() => this.handleIncrement({ id: 2 })}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+        {this.state.tags.length === 0 && " Please add data to the tag array"}
+        {this.renderTags()}
       </div>
     );
   }
