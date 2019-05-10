54146bdf24abd5c94ae90c9a5f072b5d1e8a4927
Tigran Grigoryan <tigrangrigoryan@Tigrans-MacBook-Air.local>
2019-05-10 04:01:21 +0400

    Removing the local state, multiple components in sync, lifting state up

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,60 +1,68 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = {
-    count: 0,
-    tags: ["tag1", "tag2", "tag3"]
-    // imageUrl: "http://picsum.photos/300"
-  };
+  // state = {
+  //   value: this.props.counter.value
+  //   // tags: ["tag1", "tag2", "tag3"]
+  //   // imageUrl: "http://picsum.photos/300"
+  // };
 
-  renderTags() {
-    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
+  // renderTags() {
+  //   if (this.state.tags.length === 0) return <p>There are no tags!</p>;
 
-    return (
-      <ul>
-        {this.state.tags.map(tag => (
-          <li key={tag}>{tag}</li>
-        ))}
-      </ul>
-    );
-  }
+  //   return (
+  //     <ul>
+  //       {this.state.tags.map(tag => (
+  //         <li key={tag}>{tag}</li>
+  //       ))}
+  //     </ul>
+  //   );
+  // }
 
   // constructor() {
   //   super();
   //   this.handleIncrement = this.handleIncrement.bind(this);
   // }
 
-  handleIncrement = product => {
-    console.log(product);
-    console.log("Increment Clicked", this.state.count);
-    this.setState({ count: this.state.count + 1 });
-  };
+  // handleIncrement = () => {
+  //   console.log();
+  //   console.log("Increment Clicked", this.state.value);
+  //   this.setState({ value: this.state.value + 1 });
+  // };
 
   render() {
+    console.log("props", this.props);
     //let classes = this.getBadgeClasses();
     return (
       <div>
         {/* <img src={this.state.imageUrl } alt="" /> */}
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={() => this.handleIncrement({ id: 2 })}
+          onClick={() => this.props.onIncrement(this.props.counter)}
           className="btn btn-secondary btn-sm"
         >
           Increment
         </button>
-        {this.state.tags.length === 0 && " Please add data to the tag array"}
-        {this.renderTags()}
+        {/* {this.state.tags.length === 0 && " Please add data to the tag array"} */}
+        {/* {this.renderTags()} */}
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
+        </button>
       </div>
     );
   }
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    return this.state.count === 0 ? "Zero" : this.state.count;
+    const { value } = this.props.counter;
+    return value === 0 ? "Zero" : value;
   }
 }
 
